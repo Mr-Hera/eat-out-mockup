059bf22efb28c403ea18132b702079b4a2283b34
Mr-Hera <randellhera@gmail.com>
2023-11-15 02:13:20 +0300

    Feat: added Mpesa to the project

--- app/Http/Controllers/PaymentController.php
+++ app/Http/Controllers/PaymentController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Mpesa;
 
 class PaymentController extends Controller
 {
@@ -35,6 +36,35 @@ class PaymentController extends Controller
      */
     public function stkPush(Request $request)
     {
+        $mpesa= new \Safaricom\Mpesa\Mpesa();
+
+        $BusinessShortCode = '174379';
+        $LipaNaMpesaPasskey = 'bfb279f9aa9bdbcf158e97dd71a467cd2e0c893059b10f78e6b72ada1ed2c919';
+        $TransactionType = 'CustomerPayBillOnline';
+        $Amount = '1';
+        $PartyA = '254795909989';
+        $PartyB = '174379';
+        $PhoneNumber = '254795909989';
+        $CallBackURL = 'https://yourdomain.com/mpesa/confirmation';
+        $AccountReference = 'AccountReference';
+        $TransactionDesc = 'TransactionDesc';
+        $Remarks = 'Remarks';
+
+        $stkPushSimulation=$mpesa->STKPushSimulation(
+            $BusinessShortCode,
+            $LipaNaMpesaPasskey,
+            $TransactionType,
+            $Amount,
+            $PartyA,
+            $PartyB,
+            $PhoneNumber,
+            $CallBackURL,
+            $AccountReference,
+            $TransactionDesc,
+            $Remarks
+        );
+        dd($stkPushSimulation);
+        
         return view('process');
     }
 
